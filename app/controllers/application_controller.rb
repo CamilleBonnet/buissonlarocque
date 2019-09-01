510ad8864dda3fb2e051b49df995667635a3c15d
Camille Bonnet <bonnet.cam@gmail.com>
2019-09-01 17:07:00 +0200

    fixing few first methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  helper_method :current_user, :user_signed_in?
+
+  def user_signed_in?
+    rand(2)
+  end
 end
